0f8be167134e036abd8c88d4f08df1eeaefda554
suna88 <alotofthings88@gmail.com>
2020-07-26 12:29:19 +0900

    Use Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,20 @@ void main() {
   runApp(MyApp());
 }
 
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+
+}
+
+class RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -28,7 +42,14 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: wordPair.asPascalCase),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to Flutter")
+        ),
+          body: Center(
+              child: RandomWords()
+          )
+      )
     );
   }
 }
